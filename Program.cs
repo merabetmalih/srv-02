6d4985fd5cb09f6a8bd3841ad05978d0ab520cfb
Malih Merabet <57663690+merabetmalih@users.noreply.github.com>
2024-10-04 12:20:40 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace WelcomeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my .NET application!");
+        }
+    }
+}
